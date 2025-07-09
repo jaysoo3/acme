639354fa446e2d0fc03597a6aa00e6e0d3794bd9
Jack Hsu <jack.hsu@gmail.com>
2025-07-09 16:24:09 -0400

    wip3

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,2 @@
-const a = 1';
 export * from './lib/hero';
 export * from './lib/ui';
--- packages/ui/src/lib/ui.spec.tsx
+++ packages/ui/src/lib/ui.spec.tsx
@@ -1,10 +1,5 @@
-import { render } from '@testing-library/react';
-
-import AcmeUi from './ui';
-
 describe('AcmeUi', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<AcmeUi />);
-    expect(baseElement).toBeTruthy();
+    expect(false).toBeTruthy();
   });
 });
